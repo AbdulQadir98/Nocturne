57008c44e9769d042275df2296815c98fb4617ab
AbdulQadir98 <abdul.qadirv98@gmail.com>
2023-09-18 22:32:26 +0530

    updated pipeline class

--- video-stream/pipeline.py
+++ video-stream/pipeline.py
@@ -1,6 +1,5 @@
-import threading
 from queue import Queue
-from threading import Event
+from threading import Thread, Event
 
 
 class Stage:
@@ -9,22 +8,51 @@ class Stage:
         self.buffer_size = buffer_size
         self.input_buffer = Queue(maxsize=buffer_size)
         self.output_buffer = Queue(maxsize=buffer_size)
-        self.stop_event = Event()
-        self.thread = threading.Thread(target=self.run)
+        # self.stop_event = Event()
+        self.thread = Thread(target=self.run)
         self.thread.start()
+
+    def __repr__(self):
+        return f'<Stage name={self.name}>'
 
     def run(self):
         while True:
+            # Get the next input from the input buffer.
             input_data = self.input_buffer.get()
+
+            # Check if the stage has been stopped.
+            if self.event.is_set():
+                break
+
+            # Process the input data.
             processed_data = self.process(input_data)
 
+            # Put the processed data in the output buffer.
             self.output_buffer.put(processed_data)
+
+    # def run(self):
+    #     while not self.stop_event.is_set():
+    #         # Get the next input from the input buffer.
+    #         input_data = self.input_buffer.get()
+
+    #         # Check if the stage has been stopped.
+    #         if self.stop_event.is_set():
+    #             break
+
+    #         # Process the input data.
+    #         processed_data = self.process(input_data)
+
+    #         # Put the processed data in the output buffer.
+    #         self.output_buffer.put(processed_data)
+
+    #     # Signal that the stage has finished processing data.
+    #     self.stop()
+
+    # def stop(self):
+    #     self.stop_event.set()
 
     def process(self, input_data):
         pass
-
-    def stop(self):
-        self.stop_event.set()
 
 
 class ResolutionStage(Stage):
@@ -57,50 +85,64 @@ class EnhanceStage(Stage):
     def process(self, frame):
         print("E", frame)
         processed_frame = frame
-        processed_frame = frame
         return processed_frame
 
 
 class Pipeline:
     def __init__(self, stages):
         self.stages = stages
+        # self.event = threading.Event()
         self.connect_stages()
 
     def connect_stages(self):
         for i in range(len(self.stages) - 1):
+            print(self.stages[i])
             self.stages[i].output_buffer = self.stages[i + 1].input_buffer
 
     def run(self):
+        # Start the first stage.
         self.stages[0].input_buffer.put('Start')
 
+        # Wait for the last stage to finish.
         self.stages[-1].output_buffer.get()
 
+        # Stop all of the stages.
         for stage in self.stages:
             stage.stop()
 
+        # Wait for all of the threads to finish.
         for stage in self.stages:
             stage.thread.join()
 
     def stop(self):
-        for stage in self.stages:
-            stage.stop()
+        self.event.set()
 
 
 def main():
 
+    # Create the stages in the pipeline.
     resolution_stage = ResolutionStage()
     detect_stage = DetectStage()
     enhance_stage = EnhanceStage()
 
+    # Create the pipeline.
     pipeline = Pipeline([resolution_stage, detect_stage, enhance_stage])
 
     # FOR TESTING STREAM
     data_stream = ["Frame 1", "Frame 2", "Frame 3", "Frame 4", "Frame 5"]
 
+    # Input the data stream into the pipeline
     for data in data_stream:
-        detect_stage.input_buffer.put(data)
+        resolution_stage.input_buffer.put(data)
 
     pipeline.run()
+
+    # event = threading.Event()
+    # # Trigger the event to stop the pipeline.
+    # event.set()
+    # pipeline.stop()
+
+    # Print the final frames
 
 
 if __name__ == '__main__':
