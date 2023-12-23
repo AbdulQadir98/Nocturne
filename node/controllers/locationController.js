45d73b1607bf1b4012e5cc241fb43f3253ee7d37
AbdulQadir98 <abdul.qadirv98@gmail.com>
2023-12-23 09:51:32 +0530

    updated location api

--- node/controllers/locationController.js
+++ node/controllers/locationController.js
@@ -21,3 +21,34 @@ exports.logLocation = (req, res) => {
       res.status(500).send('Internal Server Error');
     });
 };
+
+exports.getNearbyLocations = async (req, res) => {
+  const { latitude, longitude } = req.body;
+  const radius = 0.02; // Assuming 0.02 degrees is approximately 20 meters
+
+  const locationsRef = db.collection('locations');
+  const query = locationsRef.where('latitude', '>=', latitude - radius)
+    .where('latitude', '<=', latitude + radius)
+    .where('longitude', '>=', longitude - radius)
+    .where('longitude', '<=', longitude + radius);
+
+  try {
+    const snapshot = await query.get();
+    const nearbyLocations = [];
+
+    snapshot.forEach((doc) => {
+      const data = doc.data();
+      nearbyLocations.push({
+        id: doc.id,
+        latitude: data.latitude,
+        longitude: data.longitude,
+      });
+    });
+
+    logger.info('Nearby locations fetched successfully:', { latitude, longitude, nearbyLocations });
+    res.status(200).json({ nearbyLocations });
+  } catch (error) {
+    logger.error('Error fetching nearby locations:', error);
+    res.status(500).send('Internal Server Error');
+  }
+};
--- node/routes/locations.js
+++ node/routes/locations.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const locationController = require('../controllers/locationController');
 
 router.post('/api/locations', locationController.logLocation);
+router.post('/api/nearby-locations', locationController.getNearbyLocations);
 
 module.exports = router;
