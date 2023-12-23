d687fd961178d6bc96b0162cf25c38998e0a4d33
AbdulQadir98 <abdul.qadirv98@gmail.com>
2023-12-23 09:54:50 +0530

    added GeoFirestore and socketIo

--- node/server.js
+++ node/server.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 const bodyParser = require('body-parser');
+const admin = require('firebase-admin');
+const GeoFirestore = require('geofirestore');
 const routes = require('./routes');
 const logger = require('./logger');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 const port = 3000;
+
+// Initialize Firebase Admin SDK
+const serviceAccount = require('./path/to/your/serviceAccountKey.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://your-project-id.firebaseio.com',
+});
+
+// Initialize GeoFirestore with Firestore instance
+const db = admin.firestore();
+const geoFirestore = new GeoFirestore.GeoFirestore(db);
+
+app.use(bodyParser.json());
+app.use('/', routes);
 
 // Middleware for logging requests
 app.use((req, res, next) => {
@@ -12,11 +32,19 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(bodyParser.json());
-app.use('/', routes);
+// WebSocket connection handling
+io.on('connection', (socket) => {
+  logger.info(`Client connected: ${socket.id}`);
 
-app.listen(port, () => {
+  // Handle disconnection
+  socket.on('disconnect', () => {
+    logger.info(`Client disconnected: ${socket.id}`);
+  });
+});
+
+// Start the server
+server.listen(port, () => {
   logger.info(`Server is listening at http://localhost:${port}`);
 });
 
-module.exports = app;
+module.exports = { app, io, geoFirestore };  // Export the app, io, and geoFirestore instances for testing purposes
