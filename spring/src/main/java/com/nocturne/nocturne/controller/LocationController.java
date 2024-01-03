6c046ea85b4e02c924d8c5faa2984fb3d72c1443
AbdulQadir98 <abdul.qadirv98@gmail.com>
2024-01-03 10:28:45 +0530

    configure firebase

--- spring/src/main/java/com/nocturne/nocturne/controller/LocationController.java
+++ spring/src/main/java/com/nocturne/nocturne/controller/LocationController.java
@@ -1,14 +1,19 @@
 package com.nocturne.nocturne.controller;
 
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nocturne.nocturne.entity.Location;
 import com.nocturne.nocturne.service.LocationService;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/location")
@@ -18,9 +23,23 @@ public class LocationController {
     LocationService locationService;
 
     @PostMapping
-    @RequestMapping("/")
     public void saveLocation(@RequestBody Location location) {
         locationService.saveLocation(location);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Location>> getAllLocations() {
+        try {
+            List<Location> locations = locationService.getAllLocations();
+            return new ResponseEntity<>(locations, HttpStatus.OK);
+        } catch (InterruptedException | ExecutionException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/hi")
+    public String hello() {
+        return "Greetings from Spring Boot!";
     }
 
     // private final LocationService locationService;
--- spring/src/main/java/com/nocturne/nocturne/service/LocationService.java
+++ spring/src/main/java/com/nocturne/nocturne/service/LocationService.java
@@ -1,16 +1,35 @@
 package com.nocturne.nocturne.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 import org.springframework.stereotype.Service;
 
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
 import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.cloud.FirestoreClient;
 import com.nocturne.nocturne.entity.Location;
 
 @Service
 public class LocationService {
+
+    // @Autowired
+    // private Firestore firestore;
+
+    // private CollectionReference getLocationCollection() {
+    // return firestore.collection("location");
+    // }
+
+    // public ApiFuture<WriteResult> saveLocation(Location location) {
+    // ApiFuture<WriteResult> apiFuture =
+    // getLocationCollection().document(location.getId().toString()).set(location);
+    // return apiFuture;
+    // }
 
     public void saveLocation(Location location) {
         Firestore db = FirestoreClient.getFirestore();
@@ -19,5 +38,18 @@ public class LocationService {
 
         location.setId(docRef.getId());
         ApiFuture<WriteResult> apiFuture = docRef.set(location);
+    }
+
+    public List<Location> getAllLocations() throws InterruptedException, ExecutionException {
+        Firestore db = FirestoreClient.getFirestore();
+        ApiFuture<QuerySnapshot> future = db.collection("location").get();
+        List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+
+        List<Location> locations = new ArrayList<>();
+        for (QueryDocumentSnapshot document : documents) {
+            locations.add(document.toObject(Location.class));
+        }
+
+        return locations;
     }
 }
