24219001a3c5adea559289b0d6ff98c9c4215ebd
AbdulQadir98 <abdul.qadirv98@gmail.com>
2024-01-04 16:33:39 +0530

    Geofire-java

--- spring/src/main/java/com/nocturne/nocturne/controller/LocationController.java
+++ spring/src/main/java/com/nocturne/nocturne/controller/LocationController.java
@@ -45,4 +45,22 @@ public class LocationController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    // @GetMapping
+    // public void getAllLocations() {
+    // try {
+    // FirebaseDatabase database = FirebaseDatabase.getInstance();
+    // GeoFire geoFire = new GeoFire(database.getReference("locations"));
+
+    // // Perform a geo query
+    // GeoLocation center = new GeoLocation(37.7749, -122.4194);
+    // double radius = 10.0; // in kilometers
+
+    // geoFire.query().setCenter(center).setRadius(radius).addGeoQueryEventListener(new
+    // MyGeoQueryEventListener());
+    // } catch (Exception e) {
+    // e.printStackTrace();
+    // }
+    // }
+
 }
