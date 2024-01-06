121adb2d8e1b5f019bca2af78ecc633d5f71f2e2
AbdulQadir98 <abdul.qadirv98@gmail.com>
2024-01-06 23:18:34 +0530

    gitignore secret keys

--- node/config/firebase-config.js
+++ node/config/firebase-config.js
@@ -1,7 +1,6 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
-
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require("./service-account.json");
 
 initializeApp({
   credential: cert(serviceAccount)
